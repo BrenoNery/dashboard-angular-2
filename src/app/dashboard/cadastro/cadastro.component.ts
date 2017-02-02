87f3d8a722158afd1c1da77baa16eb2658f5e1e7
treinamento <treinamento>
2017-02-02 16:50:56 -0200

    Cadastro de livros completo.

--- src/app/dashboard/cadastro/cadastro.component.ts
+++ src/app/dashboard/cadastro/cadastro.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LivroService } from '../../service/livro.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Livro } from '../../model/livro';
+import { Autor } from '../../model/autor';
+import { Editora } from '../../model/editora';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,7 +13,31 @@ import { LivroService } from '../../service/livro.service';
 export class CadastroComponent {
   livros;
 
-  constructor(_livroService: LivroService) {
+  formModel: FormGroup = new FormGroup({
+    codigo: new FormControl(),
+    titulo: new FormControl(),
+    urlImagem: new FormControl()
+  });
+
+  constructor(private _livroService: LivroService) {
     this.livros = _livroService.consultarTodas();
+  }
+
+  cadastrarLivro() {
+    console.log(this.formModel.get('codigo').value);
+    console.log(this.formModel.get('titulo').value);
+    console.log(this.formModel.get('urlImagem').value);
+
+    const livro: Livro = new Livro(
+      this.formModel.get('codigo').value,
+      this.formModel.get('titulo').value,
+      new Autor(4, 'Stephen', 'King', '21/09/1947'),
+      '1950',
+      new Editora(1, 'Altas', 'Rua Teste Amorim', null),
+      this.formModel.get('urlImagem').value
+    );
+
+    console.log(this._livroService);
+    this._livroService.incluir(livro);
   }
 }
